04d53f9cfee131eef9edcb9d197c5cb18426b37d
Alican <alican.dogan@deu.edu.tr>
2019-03-18 08:25:56 +0300

    Cozümler Eklendi

---
+++ Lab02_cozum/Lab02_template/Hesap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab02_template
+{
+    public struct Koordinat
+    {
+        public int X;
+        public int Y;
+        public void Coords(int p_x, int p_y)
+        {
+            X = p_x;
+            Y = p_y;
+            //Constructor metot
+        }
+    }
+    public struct Daire
+    {
+        public double yaricap;
+         public  Koordinat K; //Sol üst köşe koordinat
+    }
+    public struct Dikdortgen
+    {
+        public double yukseklik, genislik;
+        public Koordinat K;
+
+    }
+    public class Hesap
+    {
+        public static Koordinat daire_merkez_bul(Koordinat sol_ust, double yaricap)
+        {
+            Koordinat merkez;
+            merkez.X = (int)(sol_ust.X + yaricap);
+            merkez.Y = (int)(sol_ust.Y + yaricap);
+            return merkez;
+        }
+        public static Koordinat dikdortgen_merkez_bul(Koordinat sol_ust, double yukseklik, double genislik)
+        {
+            Koordinat merkez;
+            merkez.X = (int)(sol_ust.X + (genislik/2) );
+            merkez.Y = (int)(sol_ust.Y + (yukseklik/2) ) ;
+            return merkez;
+        }
+        public static double daire_alan(double yaricap)
+        {
+            return Math.PI * Math.Pow(yaricap, 2);
+        }
+        public static double dikdortgen_alan(double genislik,double yukseklik)
+        {
+            return genislik * yukseklik;
+        }
+        public static double dikdortgen_cevre(double genislik, double yukseklik)
+        {
+            return (genislik + yukseklik)*2;
+        }
+        public static double daire_cevre(double yaricap)
+        {
+            return Math.PI*yaricap * 2;
+        }
+    }
+}
---
+++ Lab02_cozum/Lab02_template/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab02_template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire ucan_daire = new Daire();
+            Koordinat sol_ust = new Koordinat();
+            sol_ust.Coords(12, 20);
+            ucan_daire.K = sol_ust;
+            ucan_daire.yaricap = 5;
+
+            Dikdortgen dimdikdortgen = new Dikdortgen();
+            dimdikdortgen.K = sol_ust;
+            dimdikdortgen.genislik = 50;
+            dimdikdortgen.yukseklik = 30;
+
+            Console.WriteLine("Daire cevre = " + Hesap.daire_cevre(ucan_daire.yaricap));
+            Console.WriteLine("Daire alan = " + Hesap.daire_alan(ucan_daire.yaricap));
+            Console.WriteLine("Dikdörtgen cevre = " + Hesap.dikdortgen_cevre(dimdikdortgen.yukseklik,dimdikdortgen.genislik));
+            Console.WriteLine("Dikdörtgen alan = " + Hesap.dikdortgen_alan(dimdikdortgen.yukseklik, dimdikdortgen.genislik));
+
+
+
+        }
+    }
+}
